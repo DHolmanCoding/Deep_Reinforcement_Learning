4871babb6325d8a43acc2aca04a02333558f8c0a
Douglas Holman <holmando@oregonstate.edu>
2019-08-13 23:49:16 -0700

    env docstring

--- snippets/snippets.py
+++ snippets/snippets.py
@@ -1,3 +1,9 @@
+
 class Environment:
+    """
+    The environment is a model of the world that is external to the
+    agent and has the responsibility of providing observations and giving
+    rewards. It changes state based on the agent's actions
+    """
     def __init__(self):
         self.steps_left = 10
